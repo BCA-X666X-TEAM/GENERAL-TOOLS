62578766f9827387e2a388a8f57c2cc7749e96aa
MisterBlackCyber <49547632+BCA-X666X-TEAM@users.noreply.github.com>
2019-07-11 15:14:55 +0800

    Add files via upload

---
+++ X6X.sh
@@ -0,0 +1,129 @@
+#!/system/xbin/bash
+#PROGRAM: BASH SHELL
+#GA USAH RECODE YA, BIKIN MALU AJA
+#RECODE TIDAK AKAN MENJADIKAN DIRIMU SEBAGAI SEORANG PROGRAMER
+#TAPI KALO MEMPELAJARI SOURCE CODENYA SILAHKAN
+#CREATOR: MR-X666X
+#TEAM: BCA-X666X // MCC-X666X // MIDNIGHT-ATTACKER
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo "\033[34;1m   .............           "
+echo "\033[34;1m           ..,.:ccc,.      "                          
+echo "\033[34;1m          ......'''.lxO.   "                       
+echo "\033[34;1m .....''''..........,:ld;   "                        
+echo "\033[34;1m         .'.....:::.,,.x,       "                       
+echo "\033[34;1m      ..'''.            0Xxoc:,.  ...\\ "         
+echo "\033[34;1m  ....                ,ONkc.,.cokOdc',. \ "       
+echo "\033[34;1m .                   OMo           ':dd  . "       
+echo "\033[34;1m                   dMc              O:OO "       
+echo "\033[34;1m                  0M.                 :o. "   
+echo "\033[34;1m                    .Wd  "                         
+echo "\033[34;1m                     .XO," ==BLACK GENERAL====
+echo "\033[34;1m                       ,d0Odlc.,.." BY: MR-X666X
+echo "\033[34;1m                          ..',:cdOOd::,."
+echo "\033[34;1m                                    .:d;.':."
+echo "\033[34;1m                                       'd,  .'"
+echo "\033[34;1m                                         l   .."
+echo "\033[34;1m                                          .o"
+echo "\033[34;1m                                            c"
+echo "\033[34;1m                                           .'"
+echo "\033[34;1m                                            ."
+sleep 1
+echo "\033[33;1mWELCOME TO BLACK GENERAL TOOLS"
+sleep 1
+echo "\033[31;1mTHE SUPER POWER SYSTEM"
+sleep 1
+echo "\\033[34;1mINSTALLING TOOLS....."
+sleep 1
+echo "\033[31;1mWAIT...................."
+toilet -f slant -F gay "BLACK-GENERAL" | lolcat
+echo "\033[34;1m"
+cal
+date
+sleep 1
+echo "\033[31;1mwait...................."
+sleep 1
+echo "\033[31;1m                   /\  /--\  "                
+echo "\033[31;1m           _______/  \/    1 "
+echo "\033[31;1m  /\       /____            1 "
+echo "\033[31;1m  1-------/ /__|    O  /    1 "
+echo "\033[31;1m  \--------____       /    1 "
+echo "\033[31;1m   v v v v v \ \_____/  \ 1 "
+echo "\033[31;1m   /\/\/\/\/\/\ /         \ " 
+echo "\033[31;1m  \_______________      /" 
+echo "\033[34;1m   BLACK GENERAL   \    / "
+echo "\033[34;1m   THE SUPER POWER SYSTEM "
+echo "\033[34;1m   POWERED BY: MR-X666X "
+sleep 1
+echo "\033[31;1m____________________________________________ "
+echo "\033[32;1m||TOOLS : BLACK GENERAL                      ||" 
+echo "\033[32;1m||CREATOR : MR-X666X                     ||"
+echo "\033[32;1m||TEAM : BCA-X666X // MCC-X666X // MIDNIGHT-ATTACKER  || "
+echo "\033[32;1m||YOUTUBE : BlackCyberAnonymous                     || "
+echo "\033[31;1m||___________________________________________|| "
+echo "\033[33;1m[~CHOSE TOOLS :                              ||"
+echo "\033[31;1m||___________________________________________||"
+echo "\033[33;1m||                                           ||"
+echo "\033[31;1m|| [1].CRACK-FACEBOOK (GENERAL-CRACKER)          ||"
+
+echo "\033[32;1m||___________________________________________||"
+read -p "root@MR-X666X]~>" X6X
+
+if [ $X6X = 1 ] || [ $X6X = 1 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "CRACK-FACEBOOK " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+pkg install git
+pkg install python
+pkg install python2
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/BCA-X666X-TEAM/GENERAL-CRACKER
+cd GENERAL-CRACKER
+python2 X6X.py
+fi
+
+if [ $X6X = 0 ] || [ $X6X = 0 ]
+then
+echo "\033[34;1m   .............           "
+echo "\033[34;1m           ..,.:ccc,.      "                          
+echo "\033[34;1m          ......'''.lxO.   "                       
+echo "\033[34;1m .....''''..........,:ld;   "                        
+echo "\033[34;1m         .'.....:::.,,.x,       "                       
+echo "\033[34;1m      ..'''.            0Xxoc:,.  ...\\ "         
+echo "\033[34;1m  ....                ,ONkc.,.cokOdc',. \ "       
+echo "\033[34;1m .                   OMo           ':dd  . "       
+echo "\033[34;1m                   dMc              O:OO "       
+echo "\033[34;1m                  0M.                 :o. "   
+echo "\033[34;1m                    .Wd  "                         
+echo "\033[34;1m                     .XO," ==BLACK GENERAL====
+echo "\033[34;1m                       ,d0Odlc.,.." ====BY: MR-X666X===
+echo "\033[34;1m                          ..',:cdOOd::,."
+echo "\033[34;1m                                    .:d;.':."
+echo "\033[34;1m                                       'd,  .'"
+echo "\033[34;1m                                         l   .."
+echo "\033[34;1m                                          .o"
+echo "\033[34;1m                                            c"
+echo "\033[34;1m                                           .'"
+echo "\033[34;1m                                            ."
+sleep 1
+echo "\033[31;1m+÷÷÷÷[]===============> "
+toilet -f slant -F gay "BLACK-GENERAL" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+echo "\033[33;1-THANKS CAUSE USE BLACK GENERAL TOOLS"
+sleep 1
+echo "\033[33;1-PLEASE SUBSCRIBE CHANNEL YOUTUBE: BlackCyberAnonymous"
+sleep 1
+echo "\033[31;1mExit........"
+sleep 1
+exit
+fi
